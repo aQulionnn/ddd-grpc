60047f14552677509dde25e49a4b7c37efc38e94
bekasker <2004bek.asker@mail.ru>
2025-06-11 20:36:49 +0500

    feat: add server streaming for GetAllMessagesStream RPC

--- webapi/Services/MessageService.cs
+++ webapi/Services/MessageService.cs
@@ -1,12 +1,15 @@
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Microsoft.EntityFrameworkCore;
 using webapi.Entities;
 
 namespace webapi.Services;
 
-public class MessageService(AppDbContext context) : webapi.MessageService.MessageServiceBase
+public class MessageService(AppDbContext context, ILogger<MessageService> logger)
+    : webapi.MessageService.MessageServiceBase
 {
     private readonly AppDbContext _context = context;
+    private readonly ILogger<MessageService> _logger = logger;
 
     public override async Task<MessageResponse> CreateMessage(CreateMessageRequest request, ServerCallContext context)
     {
@@ -15,7 +18,7 @@ public class MessageService(AppDbContext context) : webapi.MessageService.Messag
             Id = Guid.NewGuid(),
             Description = request.Description
         };
-        
+
         await _context.Messages.AddAsync(message);
         await _context.SaveChangesAsync();
 
@@ -26,17 +29,48 @@ public class MessageService(AppDbContext context) : webapi.MessageService.Messag
         };
     }
 
-    public override async Task<MessagesResponse> GetAllMessages(GetAllMessagesRequest request, ServerCallContext context)
+    public override async Task<MessagesResponse> GetAllMessages(GetAllMessagesRequest request,
+        ServerCallContext context)
     {
         var response = new MessagesResponse();
         var messages = await _context.Messages.ToListAsync();
-        
+
         response.Items.AddRange(messages.Select(message => new MessageResponse
         {
             Id = message.Id.ToString(),
             Description = message.Description,
         }));
-        
+
         return response;
+    }
+
+    public override async Task GetAllMessagesStream(GetAllMessagesRequest request,
+        IServerStreamWriter<MessagesResponse> responseStream, ServerCallContext context)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (context.CancellationToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Request was cancelled");
+                break;
+            }
+
+            var messages = await _context.Messages.ToListAsync();
+
+            await responseStream.WriteAsync(new MessagesResponse
+            {
+                Items =
+                {
+                    messages.Select(message => new MessageResponse
+                    {
+                        Id = message.Id.ToString(),
+                        Description = message.Description,
+                    })
+                },
+                Timestamp = Timestamp.FromDateTime(DateTime.UtcNow)
+            });
+            
+            await Task.Delay(1000);
+        }
     }
 }
